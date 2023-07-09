7acfc5fd3c102d29c0dbd594cdb5a6dbf51b3463
Parker Schmitt <parkercschmitt@gmail.com>
2023-07-09 02:36:30 -0500

    Fix CICD (#72)
    No longer uses container repository to save on finances for this project; since its only one server we will upload it straight.
    In future if it gets bigger may revert.

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ import TopicRepository from './backend/repositories/TopicRepository'
 import { type CardAccountType } from './backend/types/CardAccountType'
 import Config from './backend/Config'
 import { Client } from 'pg'
+import { resourceLimits } from 'worker_threads'
 console.log(process.env) // remove this after you've confirmed it is working
 
 export const app = express()
@@ -22,78 +23,90 @@ declare module 'express-session' {
   }
 }
 
-app.use(session({
-  secret: Config.SESSION_SECRET,
-  resave: Config.SESSION_RESAVE,
-  cookie: { maxAge: Config.SESSION_COOKIE_MAX_AGE },
-  saveUninitialized: Config.SESSION_SAVE_UNINITALIZED
-}))
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const setup = async (): Promise<void> => {
+  try {
+    app.use(session({
+      secret: Config.SESSION_SECRET,
+      resave: Config.SESSION_RESAVE,
+      cookie: { maxAge: Config.SESSION_COOKIE_MAX_AGE },
+      saveUninitialized: Config.SESSION_SAVE_UNINITALIZED
+    }))
 
-const corsOptions = {
-  origin: Config.CORS_ORIGIN,
-  credentials: Config.CORS_CREDENTIALS,
-  optionsSuccessStatus: Config.CORS_OPTIONS_SUCCESS_STATUS // some legacy browsers (IE11, various SmartTVs) choke on 204
+    const corsOptions = {
+      origin: Config.CORS_ORIGIN,
+      credentials: Config.CORS_CREDENTIALS,
+      optionsSuccessStatus: Config.CORS_OPTIONS_SUCCESS_STATUS // some legacy browsers (IE11, various SmartTVs) choke on 204
+    }
+    app.use(cors(corsOptions))
+    app.use(express.json())
+
+    app.use((req: any, res: { set: (arg0: string, arg1: string) => void }, next: () => void) => {
+      res.set('Referrer-Policy', 'strict-origin-when-cross-origin')
+      res.set('Cross-Origin-Opener-Policy', 'same-origin-allow-popups')
+      next()
+    })
+
+    const client = new Client({
+      user: Config.SQL_USER,
+      host: Config.SQL_HOST,
+      database: Config.SQL_DATABASE,
+      password: Config.SQL_PASSWORD,
+      port: Config.SQL_PORT
+    })
+    await client.connect()
+
+    const accountController = new AccountController({
+      Mediator: new AccountMediator({
+        Repository: new AccountRepository({
+          tableName: Config.ACCOUNT_TABLE_NAME,
+          client
+        })
+      })
+    })
+    const topicsController = new TopicController({
+      Mediator: new TopicMediator({
+        MaxCards: Config.MAX_CARDS,
+        Repository: new TopicRepository({
+          topicTableName: Config.TOPIC_TABLE_NAME,
+          cardAccountLinkageTableName: Config.CARD_ACCOUNT_LINKAGE_NAME,
+          cardTableName: Config.CARD_TABLE_NAME,
+          cardReportTableName: Config.CARD_REPORT_TABLE_NAME,
+          client
+        })
+      })
+    })
+
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    app.post('/register', accountController.PostReceiveSignup)
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    app.post('/authenticate', accountController.PostReceiveSignin)
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    app.get('/isAuthenticated', accountController.GetIsAuthenticated)
+
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    app.get('/topics', topicsController.GetReceiveTopics)
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    app.get('/topics/:topicName/percentage', topicsController.GetReceiveTopicsPercentage)
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    app.get('/topics/:topicName/practice', topicsController.GetReceiveTopicsPractice)
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    app.post('/retrieveCards', topicsController.PostReceiveCards)
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    app.post('/saveCard', topicsController.PostSaveCard)
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    app.post('/reportCard', topicsController.PostReportCard)
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
 }
-app.use(cors(corsOptions))
-app.use(express.json())
 
-app.use((req: any, res: { set: (arg0: string, arg1: string) => void }, next: () => void) => {
-  res.set('Referrer-Policy', 'strict-origin-when-cross-origin')
-  res.set('Cross-Origin-Opener-Policy', 'same-origin-allow-popups')
-  next()
-})
-
-const client = new Client({
-  user: Config.SQL_USER,
-  host: Config.SQL_HOST,
-  database: Config.SQL_DATABASE,
-  password: Config.SQL_PASSWORD,
-  port: Config.SQL_PORT
-})
-client.connect(() => {
-  const accountController = new AccountController({
-    Mediator: new AccountMediator({
-      Repository: new AccountRepository({
-        tableName: Config.ACCOUNT_TABLE_NAME,
-        client
-      })
-    })
-  })
-  const topicsController = new TopicController({
-    Mediator: new TopicMediator({
-      MaxCards: Config.MAX_CARDS,
-      Repository: new TopicRepository({
-        topicTableName: Config.TOPIC_TABLE_NAME,
-        cardAccountLinkageTableName: Config.CARD_ACCOUNT_LINKAGE_NAME,
-        cardTableName: Config.CARD_TABLE_NAME,
-        cardReportTableName: Config.CARD_REPORT_TABLE_NAME,
-        client
-      })
-    })
-  })
-
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  app.post('/register', accountController.PostReceiveSignup)
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  app.post('/authenticate', accountController.PostReceiveSignin)
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  app.get('/isAuthenticated', accountController.GetIsAuthenticated)
-
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  app.get('/topics', topicsController.GetReceiveTopics)
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  app.get('/topics/:topicName/percentage', topicsController.GetReceiveTopicsPercentage)
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  app.get('/topics/:topicName/practice', topicsController.GetReceiveTopicsPractice)
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  app.post('/retrieveCards', topicsController.PostReceiveCards)
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  app.post('/saveCard', topicsController.PostSaveCard)
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  app.post('/reportCard', topicsController.PostReportCard)
-
+setup().then(() => {
   // start the Express server
   app.listen(Config.API_PORT, () => {
     console.log(`API server running on ${Config.API_PORT}`)
   })
+}).catch((error) => {
+  console.log(error)
 })
